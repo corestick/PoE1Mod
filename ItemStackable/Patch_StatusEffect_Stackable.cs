c7073ced48dd8d42125a49883708dd3f31534b61
DESKTOP-1F5UK32\cores <corestick@gmail.com>
2025-12-28 08:51:33 +0900

    파티원 필터, 어빌도 적용

--- ItemStackable/Patch_StatusEffect_Stackable.cs
+++ ItemStackable/Patch_StatusEffect_Stackable.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using UnityEngine;
 
 [HarmonyPatch(typeof(StatusEffect), "get_Stackable")]
 public static class Patch_StatusEffect_Stackable
@@ -6,14 +7,26 @@ public static class Patch_StatusEffect_Stackable
   [HarmonyPostfix]
   public static void Postfix(StatusEffect __instance, ref bool __result)
   {
-    if (__instance.AbilityType == GenericAbility.AbilityType.Equipment)
+    // 기본 false 유지
+    if (__result)
+      return;
+
+    if (__instance.Target != null &&
+    PartyHelper.IsPartyMember(__instance.Target))
     {
-      __result = true;
+      if (__instance.AbilityType == GenericAbility.AbilityType.Ability)
+      {
+        __result = true;
+      }
     }
 
-    // if (__instance.AbilityType == GenericAbility.AbilityType.Ring)
-    // {
-    //   __result = true;
-    // }
+    if (__instance.Owner != null &&
+    PartyHelper.IsPartyMember(__instance.Owner))
+    {
+      if (__instance.AbilityType == GenericAbility.AbilityType.Equipment)
+      {
+        __result = true;
+      }
+    }
   }
 }
