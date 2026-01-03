1ee75813666ff67f2cdd4d6a4556557befed69a1
DESKTOP-1F5UK32\cores <corestick@gmail.com>
2026-01-03 11:58:39 +0900

    억제 기준 수정

--- StackableMod/Patch_StatusEffect_Suppresses.cs
+++ StackableMod/Patch_StatusEffect_Suppresses.cs
@@ -12,73 +12,86 @@ namespace StackableMod
       bool suppress_if_tied,
       ref bool __result)
     {
-      if (!Main.Settings.EnableForAbilities)
+      if (!Main.Settings.EnableForAbilities
+          && __instance.AbilityType != GenericAbility.AbilityType.Ability)
         return true;
 
-      if (__instance == null || eff == null)
-        return true;
+      __result = false;
 
       if (eff.Params.AffectsStat == StatusEffect.ModifiedStat.GenericMarker)
       {
-        __result = false;
+        return false;
+      }
+
+      if (__instance == null || eff == null)
+      {
+        return false;
+      }
+
+      if (__instance.AfflictionOrigin != null && eff.AfflictionOrigin != null
+        && __instance.AfflictionOrigin.OverridesAffliction(eff.AfflictionOrigin))
+      {
+        __result = true;
+        return false;
+      }
+
+      if (__instance.Origin == null
+        || eff.Origin == null
+        || __instance.Params == null
+        || eff.Params == null)
+      {
         return false;
       }
 
       // 🔑 같은 능력일 때만 억제
-      if (IsSameAbilitySameEffectSameDirection(__instance, eff))
+      if (IsSameEffect(__instance, eff))
       {
-        Main.LogParams($"[Suppresses]");
-        Main.LogParams($" AbilityOrigin : {__instance.AbilityOrigin}");
-        Main.LogParams($" EffectID      : {__instance.EffectID} / {eff.EffectID}");
-        Main.LogParams($" Origin        : {__instance.CurrentAppliedValue} / {eff.CurrentAppliedValue}");
-        Main.LogParams($" Duration      : {__instance.Duration} / {eff.Duration}");
-        Main.LogParams($" TimeLeft      : {__instance.TimeLeft} / {eff.TimeLeft}");
-
-        if (__instance.HasBiggerValueThan(eff))
-        {
-          __result = true;
-          return false;
-        }
-
-        if (__instance.CurrentAppliedValue == eff.CurrentAppliedValue)
-        {
-          if (__instance.TimeLeft > eff.TimeLeft ||
-              (__instance.TimeLeft == eff.TimeLeft && suppress_if_tied))
-          {
-            __result = true;
-            return false;
-          }
-        }
+        __result = true;
       }
 
-      // 그 외는 엔진 원본 로직 사용
-      return true;
+      // 엔진 원본 로직 스킵
+      return false;
     }
 
-    static bool IsSameAbilitySameEffectSameDirection(StatusEffect a, StatusEffect b)
+    static bool IsSameEffect(StatusEffect a, StatusEffect b)
     {
-      if (a == null || b == null)
-        return false;
-
       // 1️⃣ 같은 능력
-      if (a.AbilityOrigin == null || b.AbilityOrigin == null)
-        return false;
-
-      if (a.AbilityOrigin != b.AbilityOrigin)
+      if (a.Origin != b.Origin)
         return false;
 
       // 2️⃣ 같은 스탯
       if (a.Params == null || b.Params == null)
         return false;
 
+      // 효과
       if (a.Params.AffectsStat != b.Params.AffectsStat)
         return false;
 
-      // 3️⃣ 남은 시간이 같고 다른 값
-      if (a.CurrentAppliedValue != b.CurrentAppliedValue && a.TimeLeft == b.TimeLeft)
+      // 시간이 같으면
+      if (a.TimeLeft > 0f && a.TimeLeft == b.TimeLeft)
+        return false;
+
+      // 같은 값일 경우 시간이 작은 쪽 억제
+      if (a.CurrentAppliedValue == b.CurrentAppliedValue
+        && a.TimeLeft < b.TimeLeft)
         return false;
 
       return true;
+    }
+
+    private static void TestLog(StatusEffect __instance, StatusEffect eff)
+    {
+      Main.LogParams($" << Suppresses >>");
+      Main.LogParams($" Origin                : {__instance.Origin.name} / {eff.Origin.name}");
+      Main.LogParams($" AffectsStat           : {__instance.Params.AffectsStat} / {eff.Params.AffectsStat}");
+      Main.LogParams($" EffectID              : {__instance.EffectID} / {eff.EffectID}");
+      Main.LogParams($" CurrentAppliedValue   : {__instance.CurrentAppliedValue} / {eff.CurrentAppliedValue}");
+      Main.LogParams($" TimeLeft              : {__instance.TimeLeft} / {eff.TimeLeft}");
+      Main.LogParams($" AbilityType           : {__instance.AbilityType} / {eff.AbilityType}");
+
+      Main.LogParams($" BundleName            : {__instance.BundleName} / {eff.BundleName}");
+      Main.LogParams($" GetStackingKey        : {__instance.GetStackingKey()} / {eff.GetStackingKey()}");
+      Main.LogParams($" NonstackingEffectType : {__instance.NonstackingEffectType} / {eff.NonstackingEffectType}");
     }
   }
 }
